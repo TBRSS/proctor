e8b357c3733da99ec1ed61c9b4e42dc9e81cc5e9
Paul M. Rodriguez <pmr@ruricolist.com>
2018-03-28 19:37:13 -0500

    Get tests (not suites) working.

--- package.lisp
+++ package.lisp
@@ -1,4 +1,14 @@
 ;;;; package.lisp
 
 (defpackage #:proctor
-  (:use #:cl #:alexandria #:serapeum))
+  (:use #:cl #:alexandria #:serapeum)
+  (:export #:run
+           #:define-test
+           #:define-suite
+           #:in-suite
+
+           #:is
+           #:is-true
+           #:is-false
+           #:signals
+           #:finishes))
--- proctor.lisp
+++ proctor.lisp
@@ -2,10 +2,67 @@
 
 (in-package #:proctor)
 
-(defun run (test &optional (printer (default-printer)))
+(defun run (test)
   (~>> test
        get-test-results
-       (print-test-results printer)))
+       (print-test-results test)))
+
+;; (defmacro define-suite (name &key in description)
+;;   (let ((file (test-result-file name)))
+;;     `(progn
+;;        (save-suite ',name
+;;                    :in ',in
+;;                    :description ',description)
+;;        (overlord:file-target ,name ,file ()
+;;          (run-test-to-file (find-test ',name))))))
+
+;; (defmacro in-suite (name)
+;;   `(setf (current-suite)
+;;          (find-suite ',name)))
+
+(defmacro define-test (test-name &body body)
+  (nest
+   (destructuring-bind (test-name &key in)
+       (ensure-list test-name))
+   (let ((file (test-result-file test-name))))
+   `(progn
+      (save-test ',test-name
+                 (lambda ()
+                   ,@body)
+                 :in (or ,in (current-suite)))
+      (overlord:file-target ,test-name ,file ()
+        (run-test-to-file (find-test ',test-name)
+                          ,file))
+      ',test-name)))
+
+(defmacro is (test)
+  `(is* (test-form ,test)))
+
+(defmacro is-true (test)
+  `(is (true ,test)))
+
+(defmacro is-false (test)
+  `(is (not ,test)))
+
+(defmacro signals (condition-name &body body)
+  `(signals*
+    ',condition-name
+    (test-form
+     ,@body)))
+
+(defmacro finishes (&body body)
+  `(finishes*
+    (test-form
+      ,@body)))
+
+
+
+(defgeneric run-test-to-string (test))
+(defgeneric run-test (test))
+
+(defmacro test-form (&body body)
+  `(make-test-form :form ',(if (single body) (first body) body)
+                   :function (lambda () ,@body)))
 
 (defun get-test-results (test)
   (~> test
@@ -17,6 +74,32 @@
     (overlord:build target))
   (test-result-file test))
 
+(defun make-test-target (test)
+  (test-result-file test))
+
+(defvar *test-output*
+  (make-synonym-stream '*standard-output*))
+(declaim (type output-stream *test-output*))
+
+(defun run-test-to-file (test file)
+  (let* ((result (run-test-to-result test))
+         (string
+           (with-standard-io-syntax
+             (prin1-to-string result))))
+    (overlord:write-file-if-changed string file)))
+
+(defunion test-result
+  pass
+  (failure
+   (random-state random-state)
+   (description string)))
+
+(defun run-test-to-result (test)
+  (let ((random-state (make-random-state nil)))
+    (let ((string (run-test-to-string test)))
+      (if (emptyp string) pass
+          (failure random-state string)))))
+
 (defclass abstract-test ()
   ((name
     :type symbol
@@ -25,46 +108,89 @@
    (docstring
     :type string
     :initarg :documentation
-    :reader test-documentation))
+    :initarg :description
+    :reader test-documentation)
+   (parent
+    :initform nil
+    :reader test-parent))
   (:default-initargs
    :name (required-argument :name)
-   :docstring "NO DOCS"))
+   :documentation "NO DOCS"))
 
 (defmethod print-object ((self abstract-test) stream)
   (print-unreadable-object (self stream :type t)
     (format stream "~s" (test-name self))))
 
-(defclass suite (abstract-test)
-  ((table
-    :type hash-table
-    :initform (make-hash-table)
-    :reader suite-table))
-  (:documentation "A test suite."))
+(defvar *tests* (make-hash-table))
 
-(defvar *suites*
-  (make-hash-table))
+(defun find-test (test-name)
+  (gethash (assure symbol test-name)
+           *tests*))
 
-(defun find-suite (name)
-  (gethash name *suites*))
+(defun (setf find-test) (test test-name)
+  (setf (gethash test-name *tests*)
+        (assure abstract-test test)))
 
-(defun (setf find-suite) (value name)
-  (check-type value suite)
-  (check-type name symbol)
-  (setf (gethash name *suites*) value))
+;; (defclass suite (abstract-test)
+;;   ((table
+;;     :type hash-table
+;;     :initform (make-hash-table)
+;;     :reader suite-table))
+;;   (:documentation "A test suite."))
 
-(defun ensure-suite (name &key in)
-  (check-type name symbol)
-  (assure suite
-    (let ((suite
-            (ensure2 (gethash name *suites*)
-              (make 'suite :name name))))
-      (if (null in) suite
-          (let ((parent (ensure-suite in)))
-            (setf (gethash name (suite-table parent))
-                  suite))))))
+;; (defvar *suites*
+;;   (make-hash-table))
 
-(defvar *default-suite*
-  (ensure-suite nil))
+;; (defun find-suite (name)
+;;   (or (gethash name *suites*)
+;;       (error "No such suite as ~a." name)))
+
+;; (defun (setf find-suite) (value name)
+;;   (check-type value suite)
+;;   (check-type name symbol)
+;;   (setf (gethash name *suites*) value))
+
+;; (defgeneric (setf test-parent) (parent suite)
+;;   (:method ((parent symbol) suite)
+;;     (setf (test-parent suite)
+;;           (ensure-suite parent)))
+;;   (:method (parent (suite symbol))
+;;     (setf (test-parent (ensure-suite suite))
+;;           parent))
+;;   (:method ((parent suite) (suite suite))
+;;     (with-slots (current-parent) suite
+;;       (synchronized ()
+;;         (cond ((no current-parent)
+;;                (setf current-parent parent
+;;                      (gethash (test-name suite)
+;;                               (suite-table parent))
+;;                      suite))
+;;               ((eql current-parent parent)
+;;                parent)
+;;               (t
+;;                (let ((name (test-name suite)))
+;;                  (remhash name (suite-table current-parent))
+;;                  (nix current-parent)
+;;                  (setf (test-parent suite) parent))))))))
+
+;; (defun ensure-suite (name &key in documentation)
+;;   (check-type name symbol)
+;;   (assure suite
+;;     (lret ((suite
+;;             (ensure2 (gethash name *suites*)
+;;               (make 'suite
+;;                     :name name
+;;                     :documentation documentation))))
+;;       (when in
+;;         (setf (test-parent suite) in)))))
+
+;; (defvar *default-suite*
+;;   (ensure-suite nil))
+
+(defvar *suite* nil)
+
+(defplace current-suite ()
+  *suite*)
 
 (defclass test (abstract-test)
   ((function
@@ -74,119 +200,100 @@
    (package
     :type package
     :initarg :package
-    :reader test-package))
+    :reader test-package)
+   (suite
+    :type symbol
+    :initarg :suite
+    :initarg :in
+    :reader test-suite))
   (:default-initargs
    :package *package*
    :function (required-argument :function))
   (:documentation "A single test case."))
 
 (defun make-test (name fn &key in)
-  (let* ((test (make 'test :name name :function fn))
-         (suite (find-suite in)))
-    (setf (gethash name (suite-table suite))
-          test)))
+  (make 'test :name name :function fn :in in))
 
-(defmethods test (self function package)
-  (:method run (self)
+(defun save-test (name thunk &key in)
+  (setf (find-test name)
+        (make-test name thunk :in in)))
+
+(defmethod run-test-to-string ((test test))
+  (with-slots (package name function) test
+    (with-output-to-string (*test-output*)
+      (run-test test))))
+
+(defmethod run-test ((test test))
+  (with-slots (package function) test
     (let ((*package* package))
       (funcall function))))
 
-(defclass abstract-result ()
-  ((name
-    :initarg :name
-    :type symbol
-    :reader test-name)))
+
 
-(defmethod print-object ((self abstract-result) stream)
-  (print-unreadable-object (self stream :type t)
-    (format stream "~a" (test-name self))))
+(defstruct-read-only test-form
+  (function :type function)
+  form)
 
-(defclass suite-result (abstract-result)
-  ((results
-    :type list
-    :initarg :results
-    :reader suite-test-results)))
+(defmethod print-object ((self test-form) stream)
+  (if *print-escape*
+      (call-next-method)
+      (format stream "~a" (test-form-form self))))
 
-(defgeneric test-result-plist (result))
+(defmethod run-test-form (test-form)
+  (funcall (test-form-function test-form)))
 
-(defclass test-result (abstract-result)
-  ())
+(defun is* (form)
+  (handler-case
+      (unless (run-test-form form)
+        (format *test-output*
+                "~&Test ~a failed."
+                form))
+    (serious-condition (c)
+      (format *test-output*
+              "~&Test ~a failed because of error ~a."
+              form c))))
 
-(defmethod print-object ((self test-result) stream)
-  (print-readable-object (self stream :eval t)
-    `(make ',(class-name-safe self)
-           ,@(test-result-plist self))))
+(defun signals* (condition-type form)
+  (handler-bind ((serious-condition
+                   (lambda (c)
+                     (format *test-output*
+                             "~&Test ~a failed to complete because of error ~a"
+                             form
+                             c)
+                     (return-from signals* nil)))
+                 (t
+                   (lambda (c)
+                     (when (typep c condition-type)
+                       (return-from signals* nil)))))
+    (run-test-form form))
 
-(defmethod test-result-plist ((self abstract-result))
-  nil)
+  (format *test-output*
+          "~&Test ~a completed without signaling ~a"
+          form
+          condition-type))
 
-(defclass pass (test-result)
-  ())
-
-(defmethod test-result-plist ((self pass))
-  `(:name ,(test-name self)))
-
-(defclass fail (test-result)
-  ((random-state
-    :initarg :random-state
-    :type random-state)
-   (failures
-    :initarg :failures
-    :type list)))
-
-(defmethod test-result-plist ((self fail))
-  (with-slots (random-state failures) self
-    (list :random-state random-state
-          :failures failures)))
-
-(defclass fail/abnormal (fail)
-  ((error
-    :initarg :error
-    :type error
-    :reader test-result.error)
-   (backtrace
-    :initarg :backtrace
-    :type string
-    :reader test-result.backtrace)))
-
-(defmethod test-result-plist ((self fail/abnormal))
-  (with-slots (error backtrace) self
-    (append
-     (call-next-method)
-     (list :error error
-           :backtrace backtrace))))
-
-(defclass test-result-printer ()
-  ((stream
-    :initarg :stream
-    :type stream
-    :reader printer-stream))
-  (:default-initargs
-   :stream (make-synonym-stream '*standard-output*)))
-
-(defvar *default-printer*
-  (make 'test-result-printer))
-
-(defun default-printer ()
-  *default-printer*)
-
-(defgeneric print-test-result (printer result))
-
-(defmethods test-result-printer (self stream)
-  (:method print-test-result (self (result suite-result))
-    (do-each (result (suite-test-results self))
-      (print-test-result self result)))
-  (:method print-test-result (self (result pass))
-    (format stream "."))
-  (:method print-test-result (self (result fail))
-    (format stream "~&~a failed.~%"
-            (test-name result)))
-  (:method print-test-result (self (result fail/abnormal))
-    (format stream "~%~a failed abnormally with an error of type ~a.~%"
-            (test-name result)
-            (class-name-of (test-result.error result)))))
+(defun finishes* (form)
+  (handler-bind ((serious-condition
+                   (lambda (c)
+                     (format *test-output*
+                             "~&Test ~a did not finish:~%~a"
+                             form
+                             c))))
+    (run-test-form form)))
 
 
+
+(defun print-test-results (test result)
+  (match-of test-result result
+    (pass (format t "~&Test ~a: PASS" test))
+    ((failure _ description)
+     (format t "~&Test ~a: FAIL.~%~a"
+             test
+             description))))
+
+
+
+(defconst result-ext "sexp")
 
 (defgeneric test-result-file (test)
   (:method ((test abstract-test))
@@ -198,12 +305,14 @@
            (symbol-name
              (symbol-name test)))
       (path-join
-       (overlord/cache:current-cache-dir)
+       (user-homedir-pathname)
        (make-pathname
-        :directory `(:relative "proctor" ,package-name))
-       (make-pathname
+        :directory `(:relative
+                     "proctor"
+                     ,(uiop:implementation-identifier)
+                     ,package-name)
         :name symbol-name
-        :type "sexp")))))
+        :type result-ext)))))
 
 (defun read-object-from-file (file)
   (with-input-from-file (in file :element-type 'character)
