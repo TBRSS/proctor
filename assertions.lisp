a651e32d512e8ba21ca30b648dcdabe81280e608
Paul M. Rodriguez <pmr@ruricolist.com>
2018-04-23 22:53:00 -0500

    Make debug-test do something.

--- assertions.lisp
+++ assertions.lisp
@@ -1,5 +1,17 @@
 (in-package #:proctor)
 
+(defun report (control &rest args)
+  (if *debug-test*
+      (break "~?" control args)
+      (format *test-output*
+              "~?"
+              control args)))
+
+(define-compiler-macro report (&whole call control &rest args)
+  (if (stringp control)
+      `(report (formatter ,control) ,@args)
+      call))
+
 (defstruct-read-only test-form
   (function :type function)
   form)
@@ -17,40 +29,36 @@
   (funcall (test-form-function test-form)))
 
 (defun is* (form)
-  (handler-case
-      (unless (run-test-form form)
-        (format *test-output*
-                "~&Assertion ~a failed."
-                form))
-    (serious-condition (c)
-      (format *test-output*
-              "~&Assertion ~a exited abnormally:~%~a"
-              form c))))
+  (handler-bind ((serious-condition
+                   (lambda (c)
+                     (report "~&Assertion ~a exited abnormally:~%~a"
+                             form c)
+                     (return-from is* (values)))))
+    (unless (run-test-form form)
+      (report "~&Assertion ~a failed."
+              form))))
 
 (defun signals* (condition-type form)
   (handler-bind ((serious-condition
                    (lambda (c)
-                     (if (typep c condition-type)
-                         (return-from signals* nil)
-                         (progn
-                           (format *test-output*
-                                   "~&Form ~a failed to complete because of an error of type ~a: ~a"
-                                   form
-                                   (type-of c)
-                                   c)
-                           (return-from signals* nil))))))
+                     (unless (typep c condition-type)
+                       (report "~
+~&Form ~a failed to complete because of a condition of type ~a (expected ~a): ~a"
+                               form
+                               (type-of c)
+                               condition-type
+                               c))
+                     (return-from signals* nil))))
     (run-test-form form))
 
-  (format *test-output*
-          "~&Form ~a completed without signaling ~a"
+  (report "~&Form ~a completed without signaling ~a"
           form
           condition-type))
 
 (defun finishes* (form)
   (handler-bind ((serious-condition
                    (lambda (c)
-                     (format *test-output*
-                             "~&Form ~a did not finish:~%~a"
+                     (report "~&Form ~a did not finish:~%~a"
                              form
                              c))))
     (run-test-form form)))
